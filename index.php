9391d3e9b32593f4b5b90c27c3e3de48255c19cb
Sheinrei <l.beaute@laposte.net>
2025-08-25 11:52:55 +0200

    test

--- index.php
+++ index.php
@@ -47,7 +47,7 @@ include("include/functionDb.php");
 
 <body>
     <div class="content">
-        <h2>Bienvenue <?= $_SESSION['nom_utilisateur'] ?></h2>
+        <h2>Bienvenueeeeeeeeeeeee <?= $_SESSION['nom_utilisateur'] ?></h2>
 
 
         <div class="content_intro">
